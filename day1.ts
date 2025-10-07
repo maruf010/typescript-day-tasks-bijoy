006414fe4c88470169018539da469b2e2e80a45b
maruf010 <mhbijoy013@gmail.com>
2025-10-07 12:24:48 +0600

    first commit

---
+++ day1.ts
@@ -0,0 +1,72 @@
+// Basic Type and Interface Practice
+interface User {
+  name: string;
+  email: string;
+  phone?: string;
+  active?: boolean;
+}
+
+const user1: User = {
+  name: "Maruf",
+  email: "mhbijoy013@gmail.com",
+  phone: "01713847702",
+  active: true,
+};
+console.log(user1);
+
+//Union & Type Narrowing
+function handleInput(input: number | string) {
+  if (typeof input === "number") {
+    console.log("Number Squared:", input * input);
+  } else {
+    console.log("Uppercase String:", input.toUpperCase());
+  }
+}
+handleInput(47);
+handleInput("Maruf");
+
+// Array & Object Typing
+interface Product {
+  name: string;
+  price: number;
+  inStock: boolean;
+}
+const products: Product[] = [
+  {
+    name: "Mobile",
+    price: 15000,
+    inStock: true,
+  },
+  {
+    name: "Laptop",
+    price: 80000,
+    inStock: false,
+  },
+];
+console.log(products);
+
+// Function with Generics
+function getFirstItem<T>(array: T[]): T | null {
+  return array.length > 0 ? array[0] : null;
+}
+console.log(getFirstItem([1, 2, 3]));
+
+// Readonly & Partial Utility
+interface User {
+  name: string;
+  email: string;
+  phone?: string;
+  active?: boolean;
+}
+const readonlyUser: Readonly<User> = {
+  name: "Maruf",
+  email: "maruf@example.com",
+};
+
+const updateUser: Partial<User> = {
+  phone: "0123456678",
+  active: false,
+};
+console.log(updateUser);
+
+// Type Assertion & Casting
